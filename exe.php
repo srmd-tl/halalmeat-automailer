75a99691e7f52072dd214170beade09a41e04ea4
Sarmad Sohail <sarmad-sohail@hotmail.com>
2021-09-22 12:31:33 +0500

    updates

--- exe.php
+++ exe.php
@@ -9,9 +9,8 @@ define( 'WP_USE_THEMES', false );
 # Load WordPress Core
 // Assuming we're in a subdir: "~/wp-content/plugins/current_dir"
 require_once( '../../../wp-load.php' );
-defined('BASE_PATH') or define('BASE_PATH', plugin_dir_path( __FILE__ ));
-require_once (BASE_PATH.'halalmeat-automailer.php');
-
-
-executeMainProcess('pre_order');
+defined( 'BASE_PATH' ) or define( 'BASE_PATH', plugin_dir_path( __FILE__ ) );
+require_once( BASE_PATH . 'halalmeat-automailer.php' );
+lets_do_magic();
+//executeMainProcess('order');
 //print_r(get_post(1));
